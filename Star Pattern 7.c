f81dbea9d411dc44424b5d51589b31320bbeec9a
lalitha31084 <148032121+lalitha31084@users.noreply.github.com>
2023-12-06 15:27:56 +0530

    Add files via upload

---
+++ Star Pattern 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(i = 1; i <=n; i++){
+        for(j = 1 ; j<= n; j++){
+            if(i == 5 || j == 1)
+            printf("* ");
+            else
+            printf(" ");
+        }
+        printf(" \n");
+    }
+}
