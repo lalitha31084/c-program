8314d15151a402ae46eb00240e0e369d7ede5313
lalitha31084 <148032121+lalitha31084@users.noreply.github.com>
2023-12-12 05:32:22 +0530

    Add files via upload

---
+++ Sum & Avg of Array.c
@@ -0,0 +1,18 @@
+//sum & Avg of the Array  
+#include<stdio.h>
+#define Max 50
+int main(){
+	int num[Max],n,i;
+	printf("enter size of array :: \n");
+	for(i = 0 ;i < n ; i++){
+		scanf("%d",&num[i]);
+	}
+	for(i = 0 ; i<10 ; i++){
+		if(num[i] % 2 == 0 )
+		printf("Even numbers :: %d",num[i]);
+}
+
+
+  printf("Even numbers :: %d",num[i]);
+}
+}
